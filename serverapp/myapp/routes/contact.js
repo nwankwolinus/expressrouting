6a9f792754161b60fd36a17e80647e20c2b3e171
nwankwolinus <51293362+nwankwolinus@users.noreply.github.com>
2023-05-21 14:37:40 +0100

    first commit

---
+++ serverapp/myapp/routes/contact.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('contact', { pagetitle: 'Contact Us' });
+});
+module.exports = router;
